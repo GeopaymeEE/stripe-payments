49de30ba5cf57523494be8f5950a510d9407e306
Raul Ruiz <publiux@gmail.com>
2014-12-21 10:43:52 -0500

    Update stripe_payments.php

--- catalog/controller/payment/stripe_payments.php
+++ catalog/controller/payment/stripe_payments.php
@@ -1,25 +1,30 @@
 <?php
 class ControllerPaymentStripePayments extends Controller {
 	protected function index() {
-		$this->language->load('payment/stripe_payments');
+		$this->load->language('payment/stripe_payments');
 
-		$this->data['text_credit_card'] = $this->language->get('text_credit_card');
-		$this->data['text_wait'] = $this->language->get('text_wait');
+		$data['text_credit_card'] = $this->language->get('text_credit_card');
+		$data['text_wait'] = $this->language->get('text_wait');
 
-		$this->data['entry_cc_owner'] = $this->language->get('entry_cc_owner');
-		$this->data['entry_cc_number'] = $this->language->get('entry_cc_number');
-		$this->data['entry_cc_expire_date'] = $this->language->get('entry_cc_expire_date');
-		$this->data['entry_cc_cvv2'] = $this->language->get('entry_cc_cvv2');
+		$data['entry_cc_owner'] = $this->language->get('entry_cc_owner');
+		$data['entry_cc_number'] = $this->language->get('entry_cc_number');
+		$data['entry_cc_expire_date'] = $this->language->get('entry_cc_expire_date');
+		$data['entry_cc_cvv2'] = $this->language->get('entry_cc_cvv2');
 
-		$this->data['button_confirm'] = $this->language->get('button_confirm');
-		$this->data['button_back'] = $this->language->get('button_back');
+		$data['button_confirm'] = $this->language->get('button_confirm');
+		$data['button_back'] = $this->language->get('button_back');
         
-        $this->data['stripe_payments_public_key'] = $this->config->get('stripe_payments_public_key');
+        	if ($this->config->get(stripe_payments_mode) == 'live') {
+        		$data['stripe_payments_public_key'] = $this->config->get('stripe_payments_public_key');
+        	}
+        	else {
+        		$data['stripe_payments_public_key'] = $this->config->get('stripe_payments_public_key_test');	
+        	}
 
-		$this->data['months'] = array();
+		$data['months'] = array();
 
 		for ($i = 1; $i <= 12; $i++) {
-			$this->data['months'][] = array(
+			$data['months'][] = array(
 				'text'  => strftime('%B', mktime(0, 0, 0, $i, 1, 2000)), 
 				'value' => sprintf('%02d', $i)
 			);
@@ -27,24 +32,22 @@ class ControllerPaymentStripePayments extends Controller {
 
 		$today = getdate();
 
-		$this->data['year_expire'] = array();
+		$data['year_expire'] = array();
 
 		for ($i = $today['year']; $i < $today['year'] + 11; $i++) {
-			$this->data['year_expire'][] = array(
+			$data['year_expire'][] = array(
 				'text'  => strftime('%Y', mktime(0, 0, 0, 1, 1, $i)),
 				'value' => strftime('%Y', mktime(0, 0, 0, 1, 1, $i)) 
 			);
 		}
 
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/stripe_payments.tpl')) {
-			$this->template = $this->config->get('config_template') . '/template/payment/stripe_payments.tpl';
+			return $this->load->view($this->config->get('config_template') . '/template/payment/stripe_payments.tpl', $data);
 		} else {
-			$this->template = 'default/template/payment/stripe_payments.tpl';
+			return $this->load->view('default/template/payment/stripe_payments.tpl', $data);
 		}	
-
-		$this->render();		
+		
 	}
-
 	public function send() {
 		$this->load->model('checkout/order');
 
@@ -54,10 +57,21 @@ class ControllerPaymentStripePayments extends Controller {
         
         //Load Stripe Library
         require_once('./vendor/stripe/stripe-php/lib/Stripe.php');
-        $stripe = array(
-          "secret_key"      => $this->config->get('stripe_payments_private_key'),
-          "publishable_key" => $this->config->get('stripe_payments_public_key')
-        );
+        
+        if ($this->config->get(stripe_payments_mode) == 'live') {
+		$stripe = array(
+	          "secret_key"      => $this->config->get('stripe_payments_private_key'),
+	          "publishable_key" => $this->config->get('stripe_payments_public_key')
+	        );
+       	}
+        else {
+        	$stripe = array(
+	          "secret_key"      => $this->config->get('stripe_payments_private_key_test'),
+	          "publishable_key" => $this->config->get('stripe_payments_public_key_test')
+	        );	
+	}
+        
+        
         Stripe::setApiKey($stripe['secret_key']);
         $token  = $_POST['stripeToken'];
         $error = null;
